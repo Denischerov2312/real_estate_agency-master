cc5e54ff4d8d4d1857cae126cd86a7dd680a4e97
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-03-12 20:07:39 +0300

    Добавил модель с жалобой и лайками

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Flat
+from .models import Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -8,6 +9,13 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number',)
+    raw_id_fields = ('liked_by',)
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('who_complained',)
 
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
     #новые
     new_building = models.BooleanField(blank=True, default=None, null=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name='Кто лайкнул')
     #были
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
@@ -48,3 +50,11 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Complaint(User):
+    who_complained = models.ForeignKey(Flat, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Кто пожаловался')
+    flat_adress = models.IntegerField('Квартира, на которую пожаловались',
+                                      help_text='ул. Подольских курсантов д.5 кв.4'
+                                      )
+    text = models.TextField('Текст жалобы')
