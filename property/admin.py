3e5e12353cfaa5924b8d938b767f47b9afe7e820
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-06-17 00:57:48 +0300

    Откатился к обратной версии

--- property/admin.py
+++ property/admin.py
@@ -1,21 +1,6 @@
 from django.contrib import admin
 
 from .models import Flat
-from .models import Complaint
 
 
-class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('owner', 'town', 'address',)
-    readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
-    list_editable = ('new_building',)
-    list_filter = ('new_building', 'rooms_number',)
-    raw_id_fields = ('liked_by',)
-
-
-class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ('who_complained',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
+admin.site.register(Flat)
--- property/models.py
+++ property/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
-    #новые
-    new_building = models.BooleanField(blank=True, default=None, null=True)
-    liked_by = models.ManyToManyField(User, related_name='liked_flats', verbose_name='Кто лайкнул')
-    #были
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
+
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
+
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
@@ -32,6 +29,7 @@ class Flat(models.Model):
         'Этаж',
         max_length=3,
         help_text='Первый этаж, последний этаж, пятый этаж')
+
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
         db_index=True)
@@ -40,6 +38,7 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
+
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
@@ -50,10 +49,3 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
-
-
-class Complaint(models.Model):
-    who_complained = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default=None,
-                                        verbose_name='Кто пожаловался')
-    flat_adress = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Адрес квартиры')
-    text = models.TextField('Текст жалобы')
