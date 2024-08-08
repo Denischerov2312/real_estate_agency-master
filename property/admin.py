fc0bc64290676c2ab0b6122e5e56dd0fafa017f7
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-08-08 19:37:04 +0300

    Добавил Inline связь в админке

--- property/admin.py
+++ property/admin.py
@@ -5,18 +5,29 @@ from .models import Flat
 from .models import Owner
 
 
+class Owner_flatsInline(admin.TabularInline):
+    model = Owner.flats.through
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address',)
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'owners_phonenumber', 'owner_pure_phone')
+    list_display = ('address', 'price', 'new_building', 'construction_year')
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number',)
     raw_id_fields = ('liked_by',)
+    inlines = [
+        Owner_flatsInline,
+    ]
 
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     list_display = ('name', 'pure_phonenumber')
+    inlines = [
+        Owner_flatsInline,
+    ]
+    exclude = ["flats"]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
